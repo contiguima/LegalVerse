58f668ac821c7858a2e76627d55e38e01291ae30
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2022-10-26 20:41:30 -0300

    Update Navbar.jsx

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Logo from '../../assets/img/LEGAL_VERSE_MATI-06.png';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink as BaseNavLink } from "react-router-dom";
+import Logo from "../../assets/img/LEGAL_VERSE_MATI-06.png";
 
 function navbar() {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+
+  let activeClassName = "underline";
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={Logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             
-            <Link to = "/" className={Nav.Link}>Home</Link>
-            <Nav.Link to= "/Nosotros" end>Nosotros</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -27,6 +39,7 @@ function navbar() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="/Nosotros">Nosotros</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
